8764c069c8789905e904c05d0f62f074ff6edd8e
Sebastian Steinlechner <sebastian.steinlechner@ligo.org>
2021-03-31 23:44:56 +0200

    pygwinc now allows overwriting summating function

--- ET/__init__.py
+++ ET/__init__.py
@@ -1,6 +1,7 @@
 from gwinc.ifo.noises import *
 from gwinc import load_budget
 import numpy as np
+from numpy.core.shape_base import _accumulate
 
 DEFAULT_FREQ = '1:3000:6000'
 
@@ -22,49 +23,11 @@ class ETHF(BudgetWrapper):
     ifo_name = 'ETHF' 
 
 
-def invsum(traces):
-    return 1.0/np.sum(traces, axis=0)
+def invsum(data):
+    return 1.0/np.nansum([1.0/x for x in data], axis=0)
 
-class BudgetEnvelope(nb.Budget):
-    def calc_trace(self, calibration=1, calc=True, _precomp=None):
-        """Calculate all budget noises and return BudgetTrace object
-
-        The envelope of all budgets will be given as the total noise,
-        instead of the sum of all noises.
-
-        `calibration` is always set to 1 for this calculation.
-        TODO: is that ok?
-
-        If `calc` is False, the noise will not be calculated and the
-        trace PSD will be None.  This is useful for just getting the
-        trace style info.
-
-        """
-        if _precomp is None:
-            _precomp = dict()
-
-        _cals = {}
-        if calc:
-            for name, cal in self._cal_objs.items():
-                _cals[name] = cal._calc(_precomp)
-        budget = []
-        for name in self._noise_objs:
-            trace = self.calc_noise(
-                name,
-                calibration=1.0, #TODO???
-                calc=calc,
-                _cals=_cals,
-                _precomp=_precomp,
-            )
-            budget.append(trace)
-        
-        total = invsum([1.0/trace.psd for trace in budget if trace.name in self._budget_noises])
-        return self._make_trace(
-            psd=total, budget=budget
-        )
-
-class ET(BudgetEnvelope):
-    name = 'ET-D'
+class ET(nb.Budget):
+    name = 'Einstein Telescope'
     freq = DEFAULT_FREQ
 
     noises = [
@@ -73,3 +36,5 @@ class ET(BudgetEnvelope):
     ]
     
     calibrations = [] # do not do the to-strain conversion
+
+    accumulate = invsum # calculate envelope of noise curves instead of sum
